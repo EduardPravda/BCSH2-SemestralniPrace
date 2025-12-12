9a45ade756febc95bd25b0f8ac1eeb472472ff04
JosefSmid <147180777+JosefSmid@users.noreply.github.com>
2025-12-12 23:43:25 +0100

    Clean up comments in OrdinacniDobaRepository
    Removed comments regarding SQL aliasing and case sensitivity for Oracle database.

--- OrdinaceApp/OrdinaceApp1/DataAccess/OrdinacniDobaRepository.cs
+++ OrdinaceApp/OrdinaceApp1/DataAccess/OrdinacniDobaRepository.cs
@@ -19,8 +19,6 @@ namespace OrdinaceApp1.DataAccess
             var list = new List<OrdinacniDoba>();
             using (var conn = _database.GetConnection())
             {
-                // 1. V SQL použijeme jednoduchý alias bez uvozovek: AS lekar_jmeno
-                // Oracle si to interně převede na LEKAR_JMENO
                 string sql = @"
             SELECT o.ID_Ordinacni_Doby, o.den, o.zacatek, o.konec, o.LEKAR_ID_Lekar,
                    l.prijmeni || ' ' || l.jmeno AS lekar_jmeno
@@ -36,20 +34,15 @@ namespace OrdinaceApp1.DataAccess
                         {
                             list.Add(new OrdinacniDoba
                             {
-                                // Pozor na velká/malá písmena v názvech sloupců!
-                                // U Oracle je nejbezpečnější používat VŽDY VELKÁ PÍSMENA.
 
                                 IdOrdinacniDoby = Convert.ToInt32(reader["ID_ORDINACNI_DOBY"]),
                                 Den = reader["DEN"].ToString(),
 
-                                // Načítáme jako DateTime
                                 Zacatek = Convert.ToDateTime(reader["ZACATEK"]),
                                 Konec = Convert.ToDateTime(reader["KONEC"]),
 
                                 IdLekar = Convert.ToInt32(reader["LEKAR_ID_LEKAR"]),
 
-                                // 2. Tady čteme sloupec VELKÝMI PÍSMENY: LEKAR_JMENO
-                                // Protože v SQL jsme napsali 'AS lekar_jmeno'
                                 LekarJmeno = reader["LEKAR_JMENO"].ToString()
                             });
                         }
@@ -63,14 +56,11 @@ namespace OrdinaceApp1.DataAccess
         {
             using (var conn = _database.GetConnection())
             {
-                // SQL zůstává stejné, ale parametry níže budou mít jiný typ
                 string sql = "INSERT INTO ORDINACNI_DOBA (den, zacatek, konec, LEKAR_ID_Lekar) VALUES (:den, :zac, :kon, :idLek)";
                 using (var cmd = new OracleCommand(sql, conn))
                 {
                     cmd.Parameters.Add("den", o.Den);
 
-                    // OPRAVA: Zde se odesílá objekt DateTime. 
-                    // Ovladač Oracle to automaticky převede na správný formát DATE.
                     cmd.Parameters.Add("zac", o.Zacatek);
                     cmd.Parameters.Add("kon", o.Konec);
 
